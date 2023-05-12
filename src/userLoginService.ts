ae165720a6b92265045a6b8ad8127aec606d2325
adriansoain <adriana.ansoain@540deg.com>
2023-05-12 12:13:41 +0200

    Paso 3 implementar uso del sessionManager externo y ver que el test falla

--- src/userLoginService.ts
+++ src/userLoginService.ts
@@ -1,18 +1,27 @@
-import {User} from "./user"
+import { User } from './user'
+import { FacebookSessionManager } from './facebookSessionManager'
+import { SessionManager } from './sessionManager'
 
 export class UserLoginService {
-    private loggedUsers: User[] = []
+  private loggedUsers: User[] = []
 
-    public manualLogin = (user: User): string => {
-        if(this.isUserAlreadyLogged(user)){
-            return "User already logged in"
-        }
-
-        this.loggedUsers.push(user)
-        return "User successfully logged in"
+  public manualLogin = (user: User): string => {
+    if (this.isUserAlreadyLogged(user)) {
+      return 'User already logged in'
     }
 
-    public getLoggedUsers = (): User[] => this.loggedUsers
+    this.loggedUsers.push(user)
+    return 'User successfully logged in'
+  }
 
-    private isUserAlreadyLogged = (user: User)=> this.loggedUsers.some(loggedUser => loggedUser.getUserName() === user.getUserName())
+  public getLoggedUsers = (): User[] => this.loggedUsers
+
+  public getExternalSessions = (): number => {
+    const sessionManager: SessionManager = new FacebookSessionManager()
+
+    return sessionManager.getSessions()
+  }
+
+  private isUserAlreadyLogged = (user: User) =>
+    this.loggedUsers.some(loggedUser => loggedUser.getUserName() === user.getUserName())
 }
--- test/userLogin.test.ts
+++ test/userLogin.test.ts
@@ -1,38 +1,45 @@
-import {UserLoginService} from "../src/userLoginService";
-import {User} from "../src/user";
-
+import { UserLoginService } from '../src/userLoginService'
+import { User } from '../src/user'
 
 describe('User Service Login', () => {
-    it('should log a user', () => {
-        const service = new UserLoginService()
-        const user = new User('User1')
+  it('should log a user', () => {
+    const service = new UserLoginService()
+    const user = new User('User1')
 
-        const response = service.manualLogin(user)
+    const response = service.manualLogin(user)
 
-        expect(response).toEqual('User successfully logged in')
-    })
+    expect(response).toEqual('User successfully logged in')
+  })
 
-    it('should check if user is already logged', () => {
-        const service = new UserLoginService()
-        const user = new User('User1')
-        service.manualLogin(user)
+  it('should check if user is already logged', () => {
+    const service = new UserLoginService()
+    const user = new User('User1')
+    service.manualLogin(user)
 
-       const response = service.manualLogin(user)
+    const response = service.manualLogin(user)
 
-        expect(response).toEqual('User already logged in')
-    })
+    expect(response).toEqual('User already logged in')
+  })
 
-    it('should get logged users', () => {
-        const service = new UserLoginService()
-        const user1 = new User('User1')
-        const user2 = new User('User2')
-        const expectedUsers = [user1,user2]
-        service.manualLogin(user1)
-        service.manualLogin(user2)
+  it('should get logged users', () => {
+    const service = new UserLoginService()
+    const user1 = new User('User1')
+    const user2 = new User('User2')
+    const expectedUsers = [user1, user2]
+    service.manualLogin(user1)
+    service.manualLogin(user2)
 
-       const loggedUsers = service.getLoggedUsers()
+    const loggedUsers = service.getLoggedUsers()
 
-        expect(loggedUsers).toHaveLength(2)
-        expect(loggedUsers).toEqual(expectedUsers)
-    })
+    expect(loggedUsers).toHaveLength(2)
+    expect(loggedUsers).toEqual(expectedUsers)
+  })
+
+  it('should get sessions from external service', () => {
+    const service = new UserLoginService()
+
+    const externalSessions = service.getExternalSessions()
+
+    expect(externalSessions).toEqual(7)
+  })
 })
