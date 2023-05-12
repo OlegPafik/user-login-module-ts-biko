140d559d15909d1e877d2c4c6789912b6c970344
adriansoain <adriana.ansoain@540deg.com>
2023-05-12 11:53:13 +0200

    Paso 1 Implementar logica manualLogin

--- src/userLoginService.ts
+++ src/userLoginService.ts
@@ -3,10 +3,14 @@ import {User} from "./user"
 export class UserLoginService {
     private loggedUsers: User[] = []
 
-    public manualLogin = (): string => {
-        return "user logged"
-    }
+    public manualLogin = (user: User): string => {
+        if(this.isUserAlreadyLogged(user)){
+            return "User already logged in"
+        }
 
+        this.loggedUsers.push(user)
+        return "User successfully logged in"
+    }
 
     private isUserAlreadyLogged = (user: User)=> this.loggedUsers.some(loggedUser => loggedUser.getUserName() === user.getUserName())
 }
--- test/userLogin.test.ts
+++ test/userLogin.test.ts
@@ -1,11 +1,25 @@
 import {UserLoginService} from "../src/userLoginService";
+import {User} from "../src/user";
 
 
 describe('User Service Login', () => {
-    const service = new UserLoginService()
-
     it('should log a user', () => {
-        expect(service.manualLogin()).toEqual('user logged')
+        const service = new UserLoginService()
+        const user = new User('User1')
+
+        const response = service.manualLogin(user)
+
+        expect(response).toEqual('User successfully logged in')
+    })
+
+    it('should check if user is already logged', () => {
+        const service = new UserLoginService()
+        const user = new User('User1')
+        service.manualLogin(user)
+
+       const response = service.manualLogin(user)
+
+        expect(response).toEqual('User already logged in')
     })
 
 })
